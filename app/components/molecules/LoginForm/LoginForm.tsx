4af225784f5bc4faba4ffbd8a601f6705b549556
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-25 13:05:57 +0100

    feat(login): create login form with validation

---
+++ app/components/molecules/LoginForm/LoginForm.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import styles from './LoginForm.module.scss'
+import Link from "next/link";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const LoginForm = () =>  {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmit = (data: FormData) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
+      {/* Email */}
+      <input
+        {...register("email", { required: "Email is required" })}
+        placeholder="Email"
+        className={styles.loginInputs}
+      />
+      {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+
+      {/* Password with toggle */}
+      <div className={styles.passwordWrapper}>
+        <input
+          {...register("password", { required: "Password is required" })}
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          className={styles.loginInputs}
+        />
+        <button
+          type="button"
+          className={styles.toggleButton}
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? "Hide" : "Show"}
+        </button>
+      </div>
+      {errors.password && <p className={styles.error} >{errors.password.message}</p>}
+
+
+
+        <Link href={'/'} > <p className={styles.forgotPassword}>FORGOT PASSWORD?</p> </Link>
+      <button type="submit" className={styles.submitButton}>Login</button>
+    </form>
+  );
+}
+
+export { LoginForm };
