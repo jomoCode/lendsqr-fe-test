8ad65fbea9e01a88d67993aff1a90ecec6f7faa7
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-26 00:14:54 +0100

    feat: sidebar panel

---
+++ app/components/molecules/SideBarPanel/SidebarPanel.tsx
@@ -0,0 +1,29 @@
+import styles from "./SidebarPanel.module.scss";
+import * as MyIcons from "react-icons/md";
+import clsx from "clsx";
+
+type SidebarPanelProp = {
+  icon: keyof typeof MyIcons;
+  name: string;
+  active?: boolean;
+  onClick?: () => void;
+};
+
+const SidebarPanel = ({ icon, name, active, onClick }: SidebarPanelProp) => {
+  const Icon = MyIcons[icon];
+
+  return (
+    <div
+      className={clsx(
+        styles.sideBarPanelContainer,
+        active && styles.active
+      )}
+      onClick={onClick}
+    >
+      <Icon className={styles.sideBarIcon} size={20} />
+      <p className={styles.sideBarText}>{name}</p>
+    </div>
+  );
+};
+
+export { SidebarPanel };
