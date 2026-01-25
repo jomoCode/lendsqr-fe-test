d8781881d90ed645affcfd60dfd927afd68cdd63
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-25 19:08:54 +0100

    fix: incorrect error messaege

--- app/api/auth/auth-api.ts
+++ app/api/auth/auth-api.ts
@@ -1,18 +1,22 @@
 import { apiAsync } from "./api";
 
 export const loginAsync = async (email: string, password: string) => {
-
-  if (!email || !password ) throw Error("Request must contain username and password")
-  const users: { email: string; password: string }[] = await apiAsync(
-    "https://xxxx.mockapi.io/api/v1/users",
-  );
-
-  if (users) {
-    const user = users.find(
-      (u) => u.email === email && u.password === password,
+  if (!email || !password)
+    throw Error("Request must contain username and password");
+  try {
+    const users: { email: string; password: string }[] = await apiAsync(
+      "https://xxxx.mockapi.io/api/v1/users",
     );
-    return user;
-  } else {
+
+    if (users) {
+      const user = users.find(
+        (u) => u.email === email && u.password === password,
+      );
+      return user;
+    } else {
+      return undefined;
+    }
+  } catch (error) {
     throw new Error("Invalid credentials");
   }
 };
