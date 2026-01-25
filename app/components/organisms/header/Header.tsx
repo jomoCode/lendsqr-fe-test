70ed2ef35fcc2bb615292a73fe8484a4513e33a0
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-25 20:31:46 +0100

    feat: basic header component structure with initial styling

---
+++ app/components/organisms/header/Header.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.leftContent}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={"/assets/images/logo.png"}
+            alt="company logo"
+            width={144}
+            height={30}
+            className={styles.logoImage}
+          />
+        </div>
+
+        <div className={styles.searchContainer}>search bar</div>
+      </div>
+
+
+      <div className={styles.rightContent}>
+        <Link href={"/"}>Docs</Link>
+        <div>notifiction</div>
+        <Image src="" alt={"profile image"} width={30} height={30} />
+        <div>name</div>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
