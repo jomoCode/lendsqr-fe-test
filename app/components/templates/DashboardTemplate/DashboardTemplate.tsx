9161ad6ddf8dcca2f83d1f3a53af2c9b29302856
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-27 18:20:38 +0100

    feat(Dashboard Layout): setup basic structure for the Dashbord layout

---
+++ app/components/templates/DashboardTemplate/DashboardTemplate.tsx
@@ -0,0 +1,42 @@
+import { Header } from "../../organisms/Header/Header";
+import { SideBar } from "../../organisms/SideBar/SideBar";
+import { StatusCard } from "../../organisms/StatusCard/StatusCard";
+import { statusCards } from "../../organisms/StatusCard/statusCard.data";
+import { UserTable } from "../../organisms/UserTable/UserTable";
+import styles from "./Dashboard.module.scss";
+
+const DashboardTemplate = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+
+      <div className={styles.dashboardContent}>
+        <div>
+          <SideBar />
+        </div>
+
+        <div className={styles.UserTableStatusContainer}>
+          {/* Status cards */}
+          <div className={styles.UserTableStatusWrappper}>
+            <div className={styles.dashboardStatusCard}>
+              {statusCards.map(({ color, icon, mainText, title }) => (
+                <StatusCard
+                  iconColor={color}
+                  icon={icon}
+                  mainText={mainText}
+                  title={title}
+                  key={title}
+                />
+              ))}
+            </div>
+            <div className={styles.tableWrapper}>
+              <UserTable />
+            </div>
+          </div>{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { DashboardTemplate };
