bb8d78226dd42ffa3299b73c8833f81df0bbc712
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-27 23:19:55 +0100

    feat(Dashboard page) setup dashboard pagr

---
+++ app/Dashboard/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useState } from "react";
+import styles from "./page.module.scss";
+import { DashboardTemplate } from "../components/templates/DashboardTemplate/DashboardTemplate";
+import { Header } from "../components/organisms/Header/Header";
+import { MenuButton } from "../components/atoms/MenuButton/MenuButton";
+import { SideBar } from "../components/organisms/SideBar/SideBar";
+import { statusCards } from "../components/organisms/StatusCard/statusCard.data";
+import { StatusCard } from "../components/organisms/StatusCard/StatusCard";
+import { UserTable } from "../components/organisms/UserTable/UserTable";
+;
+
+function Page() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+ 
+  return (
+    <div className={styles.container}>
+      <DashboardTemplate
+        header={<Header />}
+        menuButton={<MenuButton setSidebarOpen={()=> {setSidebarOpen(true)}}/>}
+        setSidebarOpen={setSidebarOpen}
+        sidebar={<SideBar />}
+        sidebarOpen={sidebarOpen}
+        statusCards={statusCards.map(({ color, icon, mainText, title }) => (
+          <StatusCard
+            key={title}
+            iconColor={color}
+            icon={icon}
+            mainText={mainText}
+            title={title}
+          />
+        ))}
+        userTable={<UserTable />}
+      />
+    </div>
+  );
+}
+
+export default Page;
