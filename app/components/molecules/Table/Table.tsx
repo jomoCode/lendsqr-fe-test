123ae9662e0dfddaac879e5f96b787b599945315
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-27 19:18:33 +0100

    dashboard: updated sizing styles to be more responsive

--- app/components/molecules/Table/Table.tsx
+++ app/components/molecules/Table/Table.tsx
@@ -95,7 +95,7 @@ const Table = ({ rowData, columnDefs }: TableProps) => {
         /* ===== TEXT STYLING ===== */
         .ag-header-cell-text {
           font-weight: 500 !important;
-          font-size: 14px !important;
+          font-size: 12px !important;
           line-height: 1 !important;
           color: #545f7d !important;
         }
