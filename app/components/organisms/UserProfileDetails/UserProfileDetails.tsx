7ebaa3c75cbd2be0f9757390cfcc5b902422c3a5
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-27 09:36:27 +0100

    feat: user profile details

---
+++ app/components/organisms/UserProfileDetails/UserProfileDetails.tsx
@@ -0,0 +1,16 @@
+import styles from "./UserProfileDetails.module.scss";
+import { InfoSection } from "../../molecules/InfoSection/InfoSection";
+
+type ProfileProps = {
+  sections: InfoSection[];
+};
+
+export const UserProfileDetails = ({ sections }: ProfileProps) => {
+  return (
+    <div className={styles.profileCard}>
+      {sections.map((section, index) => (
+        <InfoSection key={index} title={section.title} items={section.items} />
+      ))}
+    </div>
+  );
+};
---
+++ app/components/organisms/UserProfileDetails/userProfileDetails.data.ts
@@ -0,0 +1,46 @@
+import { InfoSection } from "../../molecules/InfoSection/InfoSection";
+
+export const profileSections: InfoSection[] = [
+  {
+    title: "Personal Information",
+    items: [
+      { label: "Full Name", value: "Grace Effiom" },
+      { label: "Phone Number", value: "07060780922" },
+      { label: "Email Address", value: "grace@gmail.com" },
+      { label: "BVN", value: "07060780922" },
+      { label: "Gender", value: "Female" },
+      { label: "Marital Status", value: "Single" },
+      { label: "Children", value: "None" },
+      { label: "Type of Residence", value: "Parent’s Apartment" },
+    ],
+  },
+  {
+    title: "Education and Employment",
+    items: [
+      { label: "Level of Education", value: "B.Sc" },
+      { label: "Employment Status", value: "Employed" },
+      { label: "Sector of Employment", value: "FinTech" },
+      { label: "Duration of Employment", value: "2 years" },
+      { label: "Office Email", value: "grace@lendsqr.com" },
+      { label: "Monthly Income", value: "₦200,000.00 - ₦400,000.00" },
+      { label: "Loan Repayment", value: "40,000" },
+    ],
+  },
+  {
+    title: "Socials",
+    items: [
+      { label: "Twitter", value: "@grace_effiom" },
+      { label: "Facebook", value: "Grace Effiom" },
+      { label: "Instagram", value: "@grace_effiom" },
+    ],
+  },
+  {
+    title: "Guarantor",
+    items: [
+      { label: "Full Name", value: "Debby Ogana" },
+      { label: "Phone Number", value: "07060780922" },
+      { label: "Email Address", value: "debby@gmail.com" },
+      { label: "Relationship", value: "Sister" },
+    ],
+  },
+];
