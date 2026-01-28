b70d2921c236e9f5f3df85d2f690013926faf67e
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-28 12:23:30 +0100

    feat:login page

---
+++ app/Auth/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { LoginLeft } from "../components/organisms/LoginLeft/LoginLeft";
+import { LoginRight } from "../components/organisms/LoginRight/LoginRight";
+import { LoginTemplate } from "../components/templates/LoginTemplate/LoginTemplate";
+import styles from "./components/templates/page.module.scss";
+
+function Page() {
+  return (
+    <div className={styles.container}>
+      <LoginTemplate left={<LoginLeft />} right={<LoginRight />} />
+    </div>
+  );
+}
+
+export default Page;
