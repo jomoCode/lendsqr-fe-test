b3a9f00e7ecd8fe0cb7c4e7d329ecbbd9727d3ab
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-28 20:59:22 +0100

    tdd: cookies- failing tests

---
+++ app/api/auth/utils/cookies/cookies.test.ts
@@ -0,0 +1,13 @@
+describe("setCookie", () => {
+  it("throws an error when given an invalid value", () => {
+    // test implementation
+  });
+
+  it("returns a cookie string when given valid inputs", () => {
+    // test implementation
+  });
+
+  it("defaults the expiry date to 7 days when no duration is provided", () => {
+    // test implementation
+  });
+});
