97ea512045ed9efb46e24110350d73190c191d49
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-29 22:48:41 +0100

    feat: updated profile details to handle empty data

--- app/components/organisms/UserProfileDetails/UserProfileDetails.tsx
+++ app/components/organisms/UserProfileDetails/UserProfileDetails.tsx
@@ -2,10 +2,11 @@ import styles from "./UserProfileDetails.module.scss";
 import { InfoSection } from "../../molecules/InfoSection/InfoSection";
 
 type ProfileProps = {
-  sections: InfoSection[];
+  sections?: InfoSection[];
 };
 
 export const UserProfileDetails = ({ sections }: ProfileProps) => {
+  if (!sections) return;
   return (
     <div className={styles.profileCard}>
       {sections.map((section, index) => (
