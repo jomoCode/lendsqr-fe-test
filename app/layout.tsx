a6d78b5dea32d02bc99c01798775e152a4593d22
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-29 12:25:09 +0100

    spread provided tanstack to view tree

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 // Google Fonts
 const geistSans = Geist({
@@ -17,7 +21,11 @@ const geistMono = Geist_Mono({
 // Local Font
 const myFont = localFont({
   src: [
-    { path: "../public/fonts/karu-regular.ttf", weight: "400", style: "normal" },
+    {
+      path: "../public/fonts/karu-regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
     { path: "../public/fonts/karu-bold.ttf", weight: "700", style: "normal" },
   ],
   variable: "--font-myfont",
@@ -32,8 +40,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geistSans.className} ${geistMono.className}`}>
-      <body className={myFont.className}>{children}</body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html
+        lang="en"
+        className={`${geistSans.className} ${geistMono.className}`}
+      >
+        <body className={myFont.className}>{children}</body>
+      </html>
+    </QueryClientProvider>
   );
 }
