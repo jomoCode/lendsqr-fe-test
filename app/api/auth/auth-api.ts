e5843a2947f4258dc2a36a2ad227fc48d7f88efb
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-29 06:59:36 +0100

    refactor and update(auth-api): update cookies after successful login

--- app/api/auth/auth-api.ts
+++ app/api/auth/auth-api.ts
@@ -1,24 +1,33 @@
-
+import { setCookie } from "../utils/cookies/cookies";
 import { apiAsync } from "./api";
 
 export const loginAsync = async (email: string, password: string) => {
   const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+  if (!baseUrl) {
+    throw new Error("API base URL not configured");
+  }
+
   if (!email || !password)
     throw Error("Request must contain username and password");
+
   try {
     const users: { email: string; password: string }[] = await apiAsync(
       `${baseUrl}/users`,
     );
 
-    if (users) {
-      const user = users.find(
-        (u) => u.email === email && u.password === password,
-      );
-      return user;
-    } else {
-      return undefined;
+    if (!users) {
+      throw new Error("Invalid credentials");
     }
+    const user = users.find(
+      (u) => u.email === email && u.password === password,
+    );
+
+    setCookie("auth_token", "mock-auth-token", 30);
+    
+    return user;
+  
   } catch (error) {
-    throw new Error("Invalid credentials");
+    throw new Error("Error signingin user");
   }
 };
