ba91bc47cb03fea1aef9713d0e51aff660b1b772
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-29 12:26:04 +0100

    setup entry point of the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-import React from "react";
-import { LoginTemplate } from "./components/templates/LoginTemplate/LoginTemplate";
-import { LoginLeft } from "./components/organisms/LoginLeft/LoginLeft";
-import { LoginRight } from "./components/organisms/LoginRight/LoginRight";
+"use client";
 
-function page() {
-  return (
-    <LoginTemplate
-      left={<LoginLeft />}
-      right={<LoginRight />}
-    />
-  );
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { getCookie } from "@/app/api/utils/cookies/cookies";
+import { Loading } from "./components/organisms/Loading/Loading";
+
+
+ const EntryPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = getCookie("auth_token");
+
+    if (token) {
+      router.replace("/dashboard");
+    } else {
+      router.replace("/auth");
+    }
+  }, [router]);
+
+  return <Loading text="Checking authentication…" />;
 }
-
-export default page;
+export default EntryPage;
