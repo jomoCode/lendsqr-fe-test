3dd53aec2abf9adc024b7cdcb9b789992af48769
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-29 12:27:27 +0100

    feat: dashboard layout

---
+++ app/pages/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import { ProtectedLayout } from "@/app/api/auth/AuthGuard";
+import { ReactNode } from "react";
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  return <ProtectedLayout>{children}</ProtectedLayout>;
+};
+export default DashboardLayout;
