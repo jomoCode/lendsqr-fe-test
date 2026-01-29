bc9895d0d7114b701b5bd3896f5f81c9b1ac0349
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-29 23:16:38 +0100

    feat: enforced automatic routing away from login screen for signed in users

--- app/(routes)/auth/page.tsx
+++ app/(routes)/auth/page.tsx
@@ -1,10 +1,20 @@
 "use client";
+import { getCookie } from "@/app/api/utils/cookies/cookies";
 import { LoginLeft } from "../../components/organisms/LoginLeft/LoginLeft";
 import { LoginRight } from "../../components/organisms/LoginRight/LoginRight";
 import { LoginTemplate } from "../../components/templates/LoginTemplate/LoginTemplate";
 import styles from "./page.module.scss";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function Page() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    const signedIn = getCookie("auth_token");
+    if (signedIn) router.push("/dashboard");
+  }, []);
+
   return (
     <div className={styles.container}>
       <LoginTemplate left={<LoginLeft />} right={<LoginRight />} />
